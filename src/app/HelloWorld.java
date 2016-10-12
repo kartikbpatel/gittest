be2188de4a9085a1fa67d634da39379ef5fd553e
kartik <patelkartik20@gmail.com>
2016-10-12 15:26:42 +0530

    switched to old code

--- src/app/HelloWorld.java
+++ src/app/HelloWorld.java
@@ -2,10 +2,6 @@ package app;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		display("Hello World!");
-	}
-
-	private static void display(String str) {
-		System.out.println(str);
+		System.out.println("Hello World!");
 	}
 }
